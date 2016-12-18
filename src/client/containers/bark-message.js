03eeaa71036a639850c1442e0ba2c2a7b2a1635d
bobjoy <bobjoy1010@163.com>
2016-12-18 13:50:11 +0800

    10 - Immutable JS 和 Redux 的改进方法

--- src/client/containers/bark-message.js
+++ src/client/containers/bark-message.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import Message from '../components/message';
 
 const mapStateToProps = state => ({
-  message: state.dog.hasBarked ? 'The dog barked' : 'The dog did not barked'
+  message: state.dog.get('hasBarked') ? 'The dog barked' : 'The dog did not barked'
 });
 
 export default connect(mapStateToProps)(Message);
